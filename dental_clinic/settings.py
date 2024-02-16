f2169f229d69c9f682aeab8ccbefe7c87a293ba0
genti.nurce <gentiannurce63@gmail.com>
2024-02-16 17:34:28 +0100

    changed static

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -111,10 +111,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -162,3 +158,5 @@ REMEMBER_ME_TTL = 604800  # 1 week
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
